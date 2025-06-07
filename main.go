173815cec09d26dd16cb205fe4b95cf09fef70c3
Kondratenko Aleksandr <madbee87@gmail.com>
2025-06-07 12:47:57 +0000

    first lesson

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Aleksandr"
+	age := 38
+	isLearningGo := true
+	height := 1.81
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("IsLearningGo:", isLearningGo)
+	fmt.Println("Height:", height)
+}
