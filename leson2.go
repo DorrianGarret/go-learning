2805b60996901cef01bf19468028eba426a120f7
Kondratenko Aleksandr <madbee87@gmail.com>
2025-06-07 16:26:47 +0000

    commit lesson2

--- leson2.go
+++ leson2.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func main() {
+	var age int
+	var language string
+
+	if age < 14 {
+		fmt.Println("Child")
+	} else if age >= 14 && age <= 17 {
+		fmt.Println("Teenager")
+	} else if age >= 18 {
+		fmt.Println("Adult")
+	}
+
+	switch language {
+	case "Go":
+		fmt.Println("Go is awesome")
+	case "Python":
+		fmt.Println("Python is cool")
+	default:
+		fmt.Println("Other language")
+	}
+}
